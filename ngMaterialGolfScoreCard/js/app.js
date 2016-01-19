3be7b060962401cc3d2861421757ffde5f435a34
jrmoser <jarodmoser@gmail.com>
2016-01-19 07:44:15 -0700

    init

---
+++ ngMaterialGolfScoreCard/js/app.js
@@ -0,0 +1,38 @@
+(function () {
+	'use strict';
+
+	angular.module('golfApp', [
+			"ui.router",
+			"map",
+			"ngMaterial",
+			"ngAnimate"
+		])
+
+		.config(["$stateProvider", "$urlRouterProvider",
+			function ($stateProvider, $urlRouterProvider) {
+				// define all app states (pages)
+				$stateProvider
+					.state("home", {
+						url: "/home",
+						templateUrl: "templates/home.html"
+					})
+					.state("list", {
+						url: "/list?listIndex",
+						templateUrl: "templates/list.html",
+						controller: "itemsController as ic"
+					})
+					.state("about", {
+						url: "/about",
+						templateUrl: "templates/about.html"
+					})
+					.state("archived", {
+						url: "/archived",
+						templateUrl: "templates/archived.html",
+						//controller: "itemsController as ic"
+					});
+
+				// if none of the above states are matched, use this as the fallback
+				$urlRouterProvider.otherwise("/about");
+			}]);
+
+}());
---
+++ ngMaterialGolfScoreCard/js/controllers/map.js
@@ -0,0 +1,19 @@
+(function () {
+	'use strict';
+
+	angular.module('map', [])
+		.controller('map', map);
+
+	map.$inject = [];
+
+	function map() {
+
+		// list everything
+		var map = this;
+
+
+		// define functions
+
+	}
+
+}());
