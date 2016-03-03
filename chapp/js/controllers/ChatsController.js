b7257c993af044dd68b9a7db57e7e48688a61366
jrmoser <jarodmoser@gmail.com>
2016-03-03 09:02:39 -0700

    empty null undefined checks

--- chapp/js/controllers/ChatsController.js
+++ chapp/js/controllers/ChatsController.js
@@ -1,61 +1,63 @@
 (function () {
-  'use strict';
+	'use strict';
 
-  angular.module('ChatsController', ['firebaseData'])
+	angular.module('ChatsController', ['firebaseData'])
 
-    .controller('ChatsController', ChatsController);
+		.controller('ChatsController', ChatsController);
 
-  ChatsController.$inject = ['$scope', '$ionicPopup', 'firebaseData', '$http'];
+	ChatsController.$inject = ['$scope', '$ionicPopup', 'firebaseData', '$http'];
 
-  function ChatsController($scope, $ionicPopup, firebaseData, $http) {
+	function ChatsController($scope, $ionicPopup, firebaseData, $http) {
 
-    var cc = this;
-    cc.addRoom = addRoom;
-    cc.addRoomPopup = addRoomPopup;
-    cc.loggedIn = firebaseData.loggedInUser.username;
-    cc.chatRooms = firebaseData.rooms;
+		var cc = this;
+		cc.addRoom = addRoom;
+		cc.addRoomPopup = addRoomPopup;
+		cc.loggedIn = firebaseData.loggedInUser.username;
+		cc.chatRooms = firebaseData.rooms;
 
-    function addRoom(name, desc) {
-      firebaseData.addRoom(name, desc);
-      cc.desc = '';
-      cc.name = '';
-    }
+		function addRoom(name, desc) {
+			firebaseData.addRoom(name, desc);
+			cc.desc = '';
+			cc.name = '';
+		}
 
-    cc.uploadFile = function(files) {
-      var fd = new FormData();
-      //Take the first selected file
-      fd.append("file", files[0]);
+		cc.uploadFile = function (files) {
+			var fd = new FormData();
+			//Take the first selected file
+			fd.append("file", files[0]);
 
-      $http.post(uploadUrl, fd, {
-        withCredentials: true,
-        headers: {'Content-Type': undefined },
-        transformRequest: angular.identity
-      }).success('...all right!...').error('..no!...');
+			$http.post(uploadUrl, fd, {
+				withCredentials: true,
+				headers: {'Content-Type': undefined},
+				transformRequest: angular.identity
+			}).success('...all right!...').error('..no!...');
 
-    };
+		};
 
 
-    function addRoomPopup() {
-      $ionicPopup.prompt({
-        title: 'Add a room that doesn\'t already exist',
-        templateUrl: 'add-chat-room.html',
-        //didn't want to, but this requires that I have the $scope used to make this work properly
-        scope: $scope,
-        buttons: [
-          {
-            text: 'Cancel',
-            type: 'button-default'
-          },
-          {
-            text: 'Add',
-            type: 'button-balanced',
-            onTap: function () {
-              cc.addRoom(cc.name, cc.desc);
-            }
-          }]
-      });
-    }
-  }
+		function addRoomPopup() {
+			$ionicPopup.prompt({
+				title: 'Add a room that doesn\'t already exist',
+				templateUrl: 'add-chat-room.html',
+				//didn't want to, but this requires that I have the $scope used to make this work properly
+				scope: $scope,
+				buttons: [
+					{
+						text: 'Cancel',
+						type: 'button-default'
+					},
+					{
+						text: 'Add',
+						type: 'button-balanced',
+						onTap: function () {
+							if (cc.name && cc.desc) {
+								cc.addRoom(cc.name, cc.desc);
+							}
+						}
+					}]
+			});
+		}
+	}
 
 
 }());
